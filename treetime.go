fcde9e0b12c97d2cfc7b5abbbbaa76e5e74bc3ae
Butty <mark@markbutterworth.net>
2016-12-14 19:01:38 +0000

    print the timestamps

--- treetime.go
+++ treetime.go
@@ -1,35 +1,49 @@
+/*
+treetime will set directory timestamps to match most recent of contents.
+
+*/
+
 package main
 
 import "fmt"
+import "os"
 import "log"
 import "flag"
 import "path/filepath"
 import "io/ioutil"
+import "time"
 
 var flagVerbose bool
 var flagDotted bool
+var flagIgnoreDir bool
 
 func walktree(droot string) (int, error) {
 
 	droot, err := filepath.Abs(droot)
 	if err != nil { log.Fatal(err) }
+	drootinfo, err := os.Stat(droot)
+	if err != nil { log.Fatal(err) }	
 
 	count := 0
-	fmt.Println("Processing", droot)
+	maxtime := time.Time{}
+
+
+	if flagVerbose { fmt.Printf("Processing %s\n", droot) }
 	flist, err := ioutil.ReadDir(droot)
 	if err != nil { log.Fatal(err) }
 
+	//first time through we check for directories recursively call this function 
   for _, i := range flist {
 
   	if i.Name()[0] == '.' {
   		if !flagDotted {
-  			fmt.Println("Skipping hidden", i.Name())
+  			if flagVerbose { fmt.Println("Skipping hidden", i.Name()) }
   			continue
   		}
   	}
   	count++
 
-  	fmt.Println(i.Name(), i.Mode().IsDir())
+  	//fmt.Println(i.Name(), i.Mode().IsDir())
 
   	if i.Mode().IsDir() {
   		dapath, err := filepath.Abs(filepath.Join(droot, i.Name()))
@@ -39,6 +53,26 @@ func walktree(droot string) (int, error) {
   	}
   }
 
+  // Must read again in case lower directories have updated time
+  if !flagIgnoreDir {
+		flist, err = ioutil.ReadDir(droot)  
+		if err != nil { log.Fatal(err) }
+
+  }
+
+	//now get most updated modification time
+  for _, i := range flist {
+  	if flagIgnoreDir && i.Mode().IsDir() { continue }
+  	if i.ModTime().After(maxtime) { maxtime = i.ModTime()}
+  }
+
+  if flagVerbose {
+  	fmt.Printf("[%s]\t%v\t-->\t%v\n", filepath.Base(droot), 
+	  	drootinfo.ModTime().Format(time.UnixDate), 
+	  	maxtime.Format(time.UnixDate))
+  }
+
+
 	return count, nil
 }
 
@@ -46,6 +80,7 @@ func walktree(droot string) (int, error) {
 func main() {
 	flag.BoolVar(&flagVerbose, "v", false, "Prints detailed operations")
 	flag.BoolVar(&flagDotted, "d", false, "Follow hidden dot directorys")
+	flag.BoolVar(&flagIgnoreDir, "i", false, "Ignore directory timestamps")
 	flag.Parse()
 
 	items := []string{"."}  // default arguments to use if omitted
