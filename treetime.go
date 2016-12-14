28eac40159cf5f7ea2eb14376e2d4f8364f8b2e6
Butty <mark@markbutterworth.net>
2016-12-14 12:32:27 +0000

    processing arguments

--- treetime.go
+++ treetime.go
@@ -2,16 +2,45 @@ package main
 
 import "fmt"
 import "flag"
+import "path/filepath"
 
 var flagVerbose bool
 
+func walktree(droot string) (int, error) {
+
+	fmt.Println("Processing", droot)
+	//err := filepath.Walk(droot, visit)
+  //fmt.Printf("filepath.Walk() returned %v\n", err)
+
+	return 1, nil
+}
+
+
 func main() {
 	flag.BoolVar(&flagVerbose, "v", false, "Prints detailed operations")
+
 	flag.Parse()
 
+	items := []string{"."}
 
 	fmt.Println("Verbose:", flagVerbose)
-	
+	fmt.Println("Args", flag.Args())
 
+	if flag.NArg() > 0 {
+		items = flag.Args()
+	}
+
+	for _,i := range items {
+		topitems, err := filepath.Glob(i)
+		if err != nil { fmt.Println(error(err))	}
+		for _,j := range topitems {
+			count, err := walktree(j)
+			if err != nil { fmt.Println(error(err))	}
+			fmt.Printf("Processed %d items in dir[%s]\n", count, i)
+		}
+
+	}
 
 }
+
+
