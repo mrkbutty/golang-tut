2feba98d657146795fac86f54cdf22008122d98d
Butty <mark@markbutterworth.net>
2016-12-15 11:20:04 +0000

    comment updates

--- treetime.go
+++ treetime.go
@@ -1,7 +1,18 @@
 /*
 treetime will set directory timestamps to match most recent of contents below.
 
+In default mode it will recursively travel down the named directorys looking at
+modification times setting the parent directory to the most recent.  This includes file and directory timestamps unless changed with "-i".
 
+Usage: treetime [directory] ...
+
+	directory = defaults to "."
+
+  -d    Follow hidden dot directorys
+  -i    Ignore directory timestamps
+  -q    No output apart from errors
+  -t    Test only do not change
+  -v    Prints detailed operations
 
 */
 
